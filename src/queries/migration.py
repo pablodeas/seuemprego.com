7c513e400b7609e0e0b975cd45deff8643ef1c6b
pablodeas <pablodeas@gmail.com>
2024-09-07 19:23:02 -0300

    updated migrations

--- src/queries/migration.py
+++ src/queries/migration.py
@@ -1,5 +1,24 @@
 import psycopg2 as psy
+import os
+from dotenv import load_dotenv
 
-with psy.connect("dbname=seuemprego user=pablodeas") as conn:
-    with conn.cursor() as cur:
-        pass
+load_dotenv()
+
+HOST = os.getenv('HOST')
+DATABASE = os.getenv('DATABASE')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+QUERIE = os.getenv('QUERIE')
+
+def exec(host, database, user, password):
+    with psy.connect(host=host, database=database, user=user, password=password) as conn:
+        with conn.cursor() as cur:
+            with open('src/queries/source.sql', 'r') as querie:
+                script = querie.read()
+                cur.execute(script)
+                result = cur.fetchall()
+                print(result)
+
+if __name__ == "__main__":
+    exec(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+
---
+++ src/queries/source.sql
@@ -0,0 +1,2 @@
+-- Let's Go
+SELECT datname FROM pg_database WHERE datname='seuemprego'
